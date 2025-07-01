baf7e495b4d3674ce7c17f707918955bdf479b08
shintaro <shintaro0825@icloud.com>
2025-07-02 02:51:35 +0900

    while文を正しい形に修正

--- src/App.java
+++ src/App.java
@@ -5,10 +5,10 @@ public class App {
     public static void main(String[] args) {
         //キーボード入力用スキャナー
         Scanner scanner = new Scanner(System.in);
-        //ランダムカードを引くためのインスタンス
+        //ランダムカードを引くためのインスタンス(クラスの実体化　初期化)　new (インスタンスを宣言)
         Random random = new Random();
 
-        //プレイヤーとCPUの引いたカードを管理するための配列（最大１０枚）
+        //プレイヤーとCPUの引いたカードを管理するための配列（最大１０枚）変数を用意
         int [] plyaerCards = new int[10];  //最大１０枚引ける
         int [] cpuCards = new int[10];
 
@@ -18,13 +18,14 @@ public class App {
 
         //プレイヤーのターンの処理
         int plyaerCardCount = 0; //プレイヤーが引いたカードの枚数
-        boolean playerBust = false; //プレイヤーがバーストしたか評価
+        boolean playerBurst = false; //プレイヤーがバーストしたか評価　初期値flaseにする　　バーストしたtrue    してないfalse
 
-        while (true){
+        while (!playerBurst){  //変数の内容を!反転  最初はtrue     バースト時　trueが！で反転してfalseでループが止まる　          trueで続く
             //カードを一枚引く
             int card = drawCard(random);
             //配列にカードの値を記録
-            plyaerCards[plyaerCardCount++] =card;
+            plyaerCards[plyaerCardCount] =card;
+            plyaerCardCount++;
             //合計点に加算
             plyaerSum += card;
 
@@ -33,25 +34,26 @@ public class App {
             System.out.println("現在の合計は" + plyaerSum + "です！");
             if (plyaerSum > 21){
                 System.out.println("バースト！あなたの負けです！");
-                playerBust = true;
-                break;
+                playerBurst = true; //
+                continue;
             }
 
             System.out.println("カードを引きますか？(yes/no)");
             String input = scanner.nextLine();
             if (input.equals("no")) {
-                break;
+                break;                                                                  //カードを引きますでnoでtrue使うと反転して意味がfalseになるとループが止まる　breakがよくてtrueがダメな理由は
             }
         }
 
         //CPUのターン
-        if(!playerBust){
+        if(!playerBurst){             //!がついているとき変数の中身自体は変わらない　　whileもifもtrueの時に中の処理を開始する
             int cpuCardCount = 0;//CPUの引いたカードの枚数を記録
 
             while (cpuSum < 17) {
                 int card = drawCard(random);
-                cpuCards[cpuCardCount++] = card;
-                cpuSum += card;
+                cpuCards[cpuCardCount] = card;    // cpuCards[0,1,2,3,4,5,6,7] cpuCards[0]=10 cpuCards[1]=20 配列にcardの値を代入
+                cpuCardCount++;
+                cpuSum += card;                   //cpuSum = cpuSum + card
 
                 System.out.println("CPUが引いたカード" + card);
             }
@@ -75,8 +77,8 @@ public class App {
         scanner.close();
     }
 
-    //試験的にメソッド化を試す
+    //試験的にカードをランダムで生成して引く処理のメソッド化
     public static int drawCard (Random random){
-        return random.nextInt(11) + 1;
+        return random.nextInt(11) + 1;       //最小値1 最大値11とする
     }
 }
